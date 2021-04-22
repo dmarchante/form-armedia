51343d2eaf42cecff314294c98bad3f860013d00
David Marchante <cdmarchante@gmail.com>
2021-04-21 21:09:59 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
+// import { ValidationService } from './validation.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form-armedia';
+  email = '';
+  plan = '';
+  date = '';
+
+  sendEmail(email: string): void {
+    this.email = email;
+  }
+
+  sendPlan(plan: string): void {
+    this.plan = plan;
+  }
+
+  sendDate(date: any): void {
+    this.date = date;
+  }
+
 }
